c2eba3b568c974a9277c436db60e500a766cce23
Seoa Kim <seoa521@gmail.com>
2024-01-26 13:45:16 +0900

    아코디언 메뉴 만들기_2

--- Practice/Accordion/custom.js
+++ Practice/Accordion/custom.js
@@ -1,9 +1,11 @@
 $(function () {
-  $('.accordion .content').eq(0).show();
-  $('.accordion .title').click(function () {
-    $(this).siblings('.accordion .content').slideUp();
+  // $('.accordion-item .content').eq(0).show();
+  $('.accordion-item .title').click(function () {
     $(this).next().stop().slideToggle(300);
     $(this).toggleClass('active');
     $(this).siblings('.accordion .title').removeClass('active');
+  });
+  $('.accordion-item .content').click(function () {
+    $(this).stop().slideUp(300);
   });
 });
